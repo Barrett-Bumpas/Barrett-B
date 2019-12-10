ddcf40d5c993a582c05714a2d6f66bb605c6130a
brbumpas <barrett.r.bumpas@gmail.com>
2019-12-10 11:43:11 -0600

    f

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const rp = require('request-promise');
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
-
-rp(url)
-  .then(function(html){
-    //success!
-    console.log(html);
-  })
-  .catch(function(err){
-    //handle error
-  });
+var toggle = false;
+function openTab(){
+  if(toggle == false){
+    document.getElementById("aboutMe").style.display = "block";
+    toggle = true;
+  } else if (toggle == true){
+    document.getElementById("aboutMe").style.display = "none";
+    toggle = false;
+  }
+}
