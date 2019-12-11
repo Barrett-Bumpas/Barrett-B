caad2eea23bcc8cc375b1fb36b3b0eca5728ef2c
brbumpas <barrett.r.bumpas@gmail.com>
2019-12-10 19:11:32 -0600

    s

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+window.onscroll = function() {scrollFunction()};
+
+var menu = document.getElementById("myMenu");
+var sticky = menu.offsetTop;
+
+function scrollFunction(){
+  if(window.pageYOffset > sticky){
+    menu.classList.add("sticky");
+  } else {
+    menu.classList.remove("sticky");
+  }
+}
+
+
+
 
 function openTab(evt, tabName){
   var i, tabContent, tabLinks;
